a817cfac69e58ab0c81f8426f95b92630d4b9250
Mokhlesur Mahin <mokhlesur.mahin@gmail.com>
2026-05-05 07:02:40 +0600

    add- tf bucket'

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -5,4 +5,10 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "eks-node-app-project-tf-bucket"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+  }
 }
